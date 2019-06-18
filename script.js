d5ec8c73c90e5266e38f12fb71233d090ad1c319
praesidios <praesidios@gmail.com>
2019-06-18 19:59:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const searchForm = document.querySelector('#search-form');
+
+
+
+function apiSearch(){
+  event.preventDefault();
+
+  const searchForm = document.querySelector('.form-control').value,
+  server = 'https://www.themoviedb.org/';
+}
+
+searchForm.addEventListener('submit', apiSearch);
+
+
+function requestApi(url) {
+
+  return(
+    url
+  )
+} 
