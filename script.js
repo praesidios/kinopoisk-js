b8dd7587ccbae863faeb5732fa18aa80d41edaf7
praesidios <praesidios@gmail.com>
2019-06-19 13:56:07 +0300

    add api

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 const searchForm = document.querySelector('#search-form');
+const movie = document.querySelector('#movies');
 
 
-
-function apiSearch(){
+function apiSearch(event){
   event.preventDefault();
 
-  const searchForm = document.querySelector('.form-control').value,
-  server = 'https://www.themoviedb.org/';
+  const searchText = document.querySelector('.form-control').value,
+  server = 'https://api.themoviedb.org/3/search/multi?api_key=edbc417414082e32c52b615ecdbab0ca&language=ru&query=---&include_adult=false' + searchText;
+  requestApi(server)
 }
 
 searchForm.addEventListener('submit', apiSearch);
 
 
 function requestApi(url) {
+  const request = new XMLHttpRequest();
 
-  return(
-    url
-  )
+  request.open('GET', url);
+  request.send();
+
+  request.addEventListener('readystatechange', function (){
+    if(request.readyState !== 4) return;
+
+    if(request.status !== 200){
+      console.log('error' + request.status);
+      return;
+    }
+
+    const output = JSON.parse(request.responseText);
+
+    let inner = '';
+    
+    output.results.forEach(function(item){
+      let nameItem = item.name || item.title;
+      inner += `<div class="col-5">${nameItem}</div>`;
+    });
+
+    movie.innerHTML = inner;
+  });
 } 
